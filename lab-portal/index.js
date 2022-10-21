ed2349910b52a0b3610e4901ab204cb843407b50
rosera <askrichardrose@gmail.com>
2022-10-21 17:20:10 +0100

    Add: Updated design and javascript

--- lab-portal/index.js
+++ lab-portal/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+// const hbs = require('handlebars');
 
 const app = express();
 const pathPublicDirectory = path.join(__dirname, './public'); 
@@ -8,7 +9,7 @@ const pathViews = path.join(__dirname, '/views');
 const pathPartials = path.join(__dirname, '/partials');
 const url = process.env.URL || 'https://storage.googleapis.com/spl-api/test.json'
 const projectId = process.env.PROJECT_ID || 'qwiklabs-gcp-01-bd4255a49d1a'
-const users = process.env.USERS || '[{"name": "user_0"}, {"name": "user_1"}, {"name": "user_3"}]'
+const users = process.env.USERS || '[{"name": "student-03-456e9711652d@qwiklabs.net"}, {"name": "user_1"}, {"name": "user_3"}]'
 const providerGoogle  = process.env.PROVIDER || '{"name": "https://accounts.google.com/AddSession?service=accountsettings&sarp=1&continue=https%3A%2F%2Fconsole.cloud.google.com%2Fhome%2Fdashboard%3Fproject%3Dqwiklabs-gcp-01-bd4255a49d1a#Email=student-03-1a34a6033750@qwiklabs.net"}'
 const googleCloudSite = "https://accounts.google.com/AddSession?service=accountsettings&sarp=1&continue=https%3A%2F%2Fconsole.cloud.google.com%2Fhome%2Fdashboard%3Fproject%3D"
 
@@ -24,6 +25,11 @@ const fetch = (...args) =>
 app.set('view engine', 'hbs');
 app.set('views', pathViews);
 hbs.registerPartials(pathPartials);
+hbs.handlebars.registerHelper('inc', function(number, options) {
+  if (typeof(number)==='undefined' || number===null) return null;
+
+  return number + (options.hash.inc || 1);
+});
 
 // Set the location of the html templates
 app.use(express.static(pathPublicDirectory));     
@@ -54,6 +60,7 @@ app.get('', async (req, res) => {
       trainers: hackathon.trainers,
       teams:hackathon.teams,
       accounts: accountsList,
+      password: "Password01",
       console: googleCloudSite 
     });
 })
